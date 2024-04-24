3d90113ec2cb1d8956b407c20cc2f3c7f39852fb
Dipesh Thapa <83170697+dipesh0078@users.noreply.github.com>
2024-04-24 20:29:46 +0545

    Add files via upload

---
+++ Hash_Table.py
@@ -0,0 +1,24 @@
+#dictionary implement hash table
+class HashTable:
+    def __init__(self):
+        self.MAX=100
+        self.arr=[None for i in range(self.MAX)]
+    def get_hash(self,key):
+     h=0
+     for char in key:
+        h+=ord(char)
+     return h%100  #100 size of list
+    
+    def add(self,key,val):
+       h=self.get_hash(key)
+       self.arr[h]=val
+    
+    def get(self,key):
+       h=self.get_hash(key)
+       return self.arr[h]
+       
+
+
+t=HashTable()
+t.add('march 6',130)
+t.get('march 6')
